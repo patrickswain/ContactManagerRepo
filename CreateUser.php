7eca1c076f0503bbd8f10e3b9505ffa8bb625a4c
joseagn <50640925+joseagn@users.noreply.github.com>
2019-05-24 18:06:09 -0400

    Add files via upload

---
+++ CreateUser.php
@@ -0,0 +1,43 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$userName = $inData["UserName"];
+	$password = $inData["Password"];
+	$firstName = $inData["FirstName"];
+	$lastName = $inData["LastName"];
+
+	$conn = new mysqli("198.71.225.55:3306", "User", "Password1", "Contacts");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$sql = "INSERT INTO `Login` (`UserName`, `Password`, `FirstName`, `LastName`) VALUES ('" . $userName . "','" . $password . "','" . $inData["FirstName"] . "','" . $inData["LastName"] . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+
+?>
---
+++ Login.php
@@ -0,0 +1,57 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+
+	$conn = new mysqli("198.71.225.55:3306", "User", "Password1", "Contacts");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$sql = "SELECT ID,FirstName,LastName FROM Login where UserName='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0)
+		{
+			$row = $result->fetch_assoc();
+			$firstName = $row["FirstName"];
+			$lastName = $row["LastName"];
+			$id = $row["ID"];
+
+			returnWithInfo($firstName, $lastName, $id );
+		}
+		else
+		{
+			returnWithError( "No Records Found" );
+		}
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"ID":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>

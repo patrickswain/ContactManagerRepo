b3b46952d0a0bf08a1c8ba5794c7875711fe66c8
patrickswain <patrickrswain@gmail.com>
2019-05-24 16:58:03 -0400

    Update javacode.js

--- javacode.js
+++ javacode.js
@@ -3,7 +3,7 @@ var url = "contactmngr.com";
 
 function login()
 {
-
+	var url = "contactmngr.com/API/Login.php";
 }
 
 function logout()
@@ -11,9 +11,20 @@ function logout()
 
 }
 
+function addUser()
+{
+	var url = "contactmngr.com/API/CreateUser.php";
+}
+
 function addContact()
 {
+	// Take in contact's name
+	var contact_name = document.getElementById("new_contact_name").value;
+	// Set result intdicator to blank
+	document.getElementById("contact_added_result") = "";
 
+	var jsonText = "{}"
+	var url = "contactmngr.com/API/AddContact.php";
 }
 
 function deleteContact()

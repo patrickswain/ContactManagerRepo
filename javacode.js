05478105a9fbd462655b5d85840d2195616f4a28
patrickswain <patrickrswain@gmail.com>
2019-05-24 17:41:35 -0400

    Update javacode.js

--- javacode.js
+++ javacode.js
@@ -20,15 +20,42 @@ function addUser()
 
 function addContact()
 {
-	// Take in contact's name
+	// Take in contact's info
 	var contact_first_name = document.getElementById("new_contact__first_name").value;
 	var contact_last_name = document.getElementById("new_contact_last_name").value;
+	var contact_email = document.getElementById("new_contact_email").value;
+	var contact_phone = document.getElementById("new_contact_phone").value;
 
 	// Set result intdicator to blank
 	document.getElementById("contact_added_result") = "";
 
-	var jsonText = "{}"
+	var jsonText = '{"FirstName" : "' + contact_first_name + '",
+			              "LastName" : "' + contact_last_name + '",
+										"User_ID"  : "' + USER_ID + '",
+										"PhoneNumber" : "' + contact_phone + '",
+										"Email" : "' + contact_email + '",
+										"Address" : "' + contact_address +  '}';
+
+	// Connect to API
 	var url = "contactmngr.com/API/AddContact.php";
+	var xmlhr = new XMLHttpRequest();
+	xmlhr.open("POST", url, true);
+	xmlhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try {
+		xmlhr.send(jsonText);
+		xmlhr.onreadystatechange =function(){
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("contact_added_result").innerHTML = "Contact Added";
+			}
+		};
+
+	} catch (e) {
+		document.getElementById("contact_added_result").innerHTML = e.message;
+	}
+
+
 }
 
 function deleteContact()

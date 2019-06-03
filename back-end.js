51c109e830e71afc3ca7f49eea498b8d0480b1ed
patrickswain <patrickrswain@gmail.com>
2019-06-02 23:42:26 -0400

    uncommented addContact

--- back-end.js
+++ back-end.js
@@ -98,44 +98,43 @@ function addUser()
 	}
 }
 
-// function addContact()
-// {
-// 	// Take in contact's info
-// 	var contact_first_name = document.getElementById("new_contact_first_name").value;
-// 	var contact_last_name = document.getElementById("new_contact_last_name").value;
-// 	var contact_email = document.getElementById("new_contact_email").value;
-// 	var contact_phone = document.getElementById("new_contact_phone").value;
-//
-//
-// 	// Set result intdicator to blank
-// 	document.getElementById("contact_added_result").innerHTML = "";
-//
-// 	var jsonText = '{"FirstName" : "' + contact_first_name + '","LastName" : "' + contact_last_name + '","User_ID"  : "' + USER_ID + '","PhoneNumber" : "' + contact_phone + '","Email" : "' + contact_email + '","Address" : "' + contact_address '}';
-//
-// 	// Connect to API
-// 	var url = "/API/AddContact.php";
-// 	var xmlhr = new XMLHttpRequest();
-// 	xmlhr.open("POST", url, true);
-// 	xmlhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-//
-// 	// Send jsonText to API
-// 	try {
-// 		xmlhr.send(jsonText);
-// 		xmlhr.onreadystatechange =function(){
-// 			if (this.readyState == 4 && this.status == 200)
-// 			{
-// 				// Update HTML
-// 				document.getElementById("contact_added_result").innerHTML = "Contact Added";
-// 			}
-// 		};
-//
-// 	} catch (e) {
-// 		// Update HTML
-// 		document.getElementById("contact_added_result").innerHTML = e.message;
-// 	}
-//
-//
-// }
+function addContact()
+{
+	// Take in contact's info
+	var contact_first_name = document.getElementById("new_contact_first_name").value;
+	var contact_last_name = document.getElementById("new_contact_last_name").value;
+	var contact_email = document.getElementById("new_contact_email").value;
+	var contact_phone = document.getElementById("new_contact_phone").value;
+
+	// Set result intdicator to blank
+	document.getElementById("contact_added_result").innerHTML = "";
+
+	var jsonText = '{"FirstName" : "' + contact_first_name + '","LastName" : "' + contact_last_name + '","User_ID"  : "' + USER_ID + '","PhoneNumber" : "' + contact_phone + '","Email" : "' + contact_email + '","Address" : "' + contact_address '}';
+
+	// Connect to API
+	var url = "/API/AddContact.php";
+	var xmlhr = new XMLHttpRequest();
+	xmlhr.open("POST", url, true);
+	xmlhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	// Send jsonText to API
+	try {
+		xmlhr.send(jsonText);
+		xmlhr.onreadystatechange =function(){
+			if (this.readyState == 4 && this.status == 200)
+			{
+				// Update HTML
+				document.getElementById("contact_added_result").innerHTML = "Contact Added";
+			}
+		};
+
+	} catch (e) {
+		// Update HTML
+		document.getElementById("contact_added_result").innerHTML = e.message;
+	}
+
+
+}
 
 function deleteContact()
 {

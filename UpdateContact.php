cfdbd768309575973eff8e7b081ab463cf4929bf
joseagn <50640925+joseagn@users.noreply.github.com>
2019-06-04 18:25:08 -0400

    Create UpdateContact.php

---
+++ UpdateContact.php
@@ -0,0 +1,47 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$userID= $inData["User_ID"];
+	$firstName = $inData["FirstName"];
+	$lastName = $inData["LastName"];
+	$phoneNumber = $inData["PhoneNumber"];
+	$email = $inData["Email"];
+	$address = $inData["Address"];
+
+
+	$conn = new mysqli("198.71.225.55:3306", "User", "Password1!", "Contacts");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+    $sql = "UPDATE ContactInfo SET FirstName='" . $firstName . "', LastName='" . $lastName . "', PhoneNumber='" . $phoneNumber . "', Email='" . $email . "', Address='" . $address . "' WHERE Contact_ID='" . $inData["id"] . "'";
+		$sql = "INSERT INTO `ContactInfo` (`FirstName`, `LastName`, `User_ID`, `PhoneNumber`, `Email`, `Address` ) VALUES ('" . $firstName . "','" . $lastName . "','" . $userID. "','" . $phoneNumber . "','" . $email . "','" . $address . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+
+?>

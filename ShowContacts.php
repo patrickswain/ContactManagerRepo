c74668d228a85b8a5e044eee8b17ea9b994aba83
patrickswain <patrickrswain@gmail.com>
2019-06-08 14:32:30 -0400

    trying to fix user id error

--- ShowContacts.php
+++ ShowContacts.php
@@ -4,7 +4,8 @@
 
   $inData = getRequestInfo();
 
-  $userID= "_SESSION['User_ID']";
+  //$userID= "_SESSION['User_ID']";
+  userID = $inData["userID"];
   $firstName = "";
   $lastName = "";
   $phoneNumber = "";
@@ -25,7 +26,7 @@
     $contacts = array();
 
     mysqli_num_rows($result);
-    
+
 		if (mysqli_num_rows($result) > 0)
 		{
       while($row = mysqli_fetch_assoc($result))
